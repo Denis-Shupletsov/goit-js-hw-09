445b307398266b12c460f9fd4b4a2b4df8615c11
Denis-Shupletsov <b128k2351763@gmail.com>
2023-04-10 08:33:25 +0300

    1ok

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
 
+const bodyEl = document.querySelector('body');
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+
+stopEl.disabled = true;
+let intervalID = null;
+
+const generatorRandomColors = {
+    DELAY: 1000,
+
+    getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    },
+
+    interval() {
+        intervalID = setInterval(() => {
+            randomChangeColor();
+        }, this.DELAY);
+        stopEl.disabled = false;
+    },
+
+    start() {
+        startEl.addEventListener('click', () => {
+            this.interval();
+            startEl.disabled = true;
+            stopEl.disabled = false;
+        });
+        stopEl.addEventListener('click', this.stop);
+    },
+
+    stop() {
+        clearInterval(intervalID);
+        stopEl.disabled = true;
+        startEl.disabled = false;
+    },
+};
+
+function randomChangeColor() {
+    bodyEl.style.backgroundColor = `${generatorRandomColors.getRandomHexColor()}`;
+}
+
+generatorRandomColors.start();
